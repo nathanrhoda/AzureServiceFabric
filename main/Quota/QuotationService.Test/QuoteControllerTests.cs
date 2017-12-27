3664fa4ce00fbdf1ff694baee29d592ad3e61a23
nathanrhoda <nathan.rhoda@gmail.com>
2017-12-27 16:03:59 +0200

    Structures and references created for creation of Quotation api calls and tests

---
+++ main/Quota/QuotationService.Test/QuoteControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuotationService.Controllers;
+
+namespace QuotationService.Test
+{
+    [TestClass]
+    public class QuoteControllerTests
+    {
+        [TestMethod]
+        public void GenerateQuote_WhereValidDataIsSupplied_ReturnsQuotationDetailsCapturedSuccessfullyMessage()
+        {
+            var controller = new QuotesController();
+            var quotes = controller.Get();
+            Assert.IsNotNull(quotes);
+        }
+    }
+}
