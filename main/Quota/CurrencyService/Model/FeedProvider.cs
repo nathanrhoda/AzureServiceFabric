fcf46d7c3316350f909cb3ae27d0c3e8df8cb607
nathanrhoda <nathan.rhoda@gmail.com>
2017-12-27 12:44:27 +0200

    Added empty quotation service statefull service and test project

--- main/Quota/CurrencyService/Model/FeedProvider.cs
+++ main/Quota/CurrencyService/Model/FeedProvider.cs
@@ -6,7 +6,7 @@ namespace CurrencyService.Model
     {
         public CurrencyFeed GetFeed()
         {
-            var feed = APIUtilities.Get<CurrencyFeed>(http://www.apilayer.net/api/live?access_key=<ENTER YOUR ACCESS KEY>");
+            var feed = APIUtilities.Get<CurrencyFeed>("http://www.apilayer.net/api/live?access_key=4633a6c0baf8136ca5b9d56a09cce755");
             return feed;
            
         }
